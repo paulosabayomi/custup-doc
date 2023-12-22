ae0cc651cd59bc1352cca06d5d699078c10039b6
Abayomi Amusa <amusaabayomipaul99@gmail.com>
2023-12-22 17:25:52 +0100

    updated the edit github url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/paulosabayomi/custup-doc-files/tree/main/',
+            'https://github.com/paulosabayomi/custup-doc/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/paulosabayomi/custup-doc-files/tree/main/',
+            'https://github.com/paulosabayomi/custup-doc/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
